feeccfbeb6dbc0248b0cda3ee33e1a3786adf753
Gaston Dandre <gaston.dandre@gmail.com>
2020-07-26 14:23:17 -0300

    Inicio de resultados de busqueda en mobile

--- js/index.js
+++ js/index.js
@@ -68,7 +68,6 @@ function busqueda() {
     let buscar = search.value;
     //Si en el input no hay nada escrito deja vacio el <ul> y quita la clase menu-activo para no modificar el DOM, si no realiza la busqueda
     if (buscar.length > 0) {
-        let apiKey = 'Jfllnbj2B6JMigULbuJvipUj8bsKh4l4'
         var buscando = $.get(`https://api.giphy.com/v1/tags/related/${buscar}?api_key=${apiKey}&limit=4&rating=g`);
         buscando.done(function (data) { getData(data); });
     } else {
@@ -76,8 +75,6 @@ function busqueda() {
         listadoSearch.classList.remove("menu-activo");
     }
 }
-
-
 
 //Esta funcion obtiene los valores buscados en GIPHY y los muestra en el DOM, agregando los <li> en el <ul> del buscador
 function getData(data) {
@@ -92,4 +89,41 @@ function getData(data) {
 //Capturar el evento click de la lista creada con sugerencias para el autocompletado
 document.getElementById("lista_search").addEventListener("click", function (e) {
     search.value = e.target.textContent;
+    listadoSearch.innerHTML = "";
+    listadoSearch.classList.remove("menu-activo");
 })
+
+//Llamado de funcion desde un click en lupa o enter
+
+document.getElementById("buscador").addEventListener("keypress", function (e) {
+    if (e.key === "Enter") {
+        listadoSearch.innerHTML = "";
+        listadoSearch.classList.remove("menu-activo");
+        getBusquedaGiphy();
+    }
+})
+
+document.getElementById("lupa").addEventListener("click", () => {
+    listadoSearch.innerHTML = "";
+    listadoSearch.classList.remove("menu-activo");
+    getBusquedaGiphy();
+});
+
+function getBusquedaGiphy() {
+    var busquedaGifs = $.get(`http://api.giphy.com/v1/gifs/search?q=${search.value}&api_key=${apiKey}`);
+    busquedaGifs.done(function (data) { muestraBusqueda(data); });
+}
+
+function muestraBusqueda(data) {
+    debugger;
+    let valorBusqueda = data.data;
+    let sectorBusqueda = document.getElementById("busqueda_section");
+    sectorBusqueda.classList.add("busqueda-section");
+    let listaGifs = "";
+    for (let i = 0; i < valorBusqueda.length; i++) {
+        let gifElement = valorBusqueda[i];
+        listaGifs += `<div class="gif-card"><img class="gif-img" src="${gifElement.images.original.url}"></div>`;
+        sectorBusqueda.innerHTML = listaGifs;
+        console.log(sectorBusqueda);
+    }
+}
