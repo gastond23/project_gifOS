7d194e353d6d85fb0733f74b98c9b9382bd29c9e
Gaston Dandre <gaston.dandre@gmail.com>
2020-08-23 01:37:13 -0300

    agregando estilos a la grabación del gif, falta la etapa de pausa y subida

--- js/crear-gifo.js
+++ js/crear-gifo.js
@@ -17,9 +17,15 @@ let pasos = document.getElementsByClassName("button-number");
 let stream, recorder;
 let form = new FormData();
 let apiKey = "Jfllnbj2B6JMigULbuJvipUj8bsKh4l4";
+let mensajesCrearGifo = document.getElementsByClassName("text-gifo-container");
+let timer = document.getElementById("timer");
+let comienzo;
+let repeatGif = document.getElementById("repeatGif");
 
 function activateCamera() {
     debugger;
+    mensajesCrearGifo[0].classList.remove("active");
+    mensajesCrearGifo[1].classList.add("active");
     buttonIni.style.display = "none";
     buttonRec.style.display = "block";
     pasos[0].classList.add("button-act");
@@ -48,29 +54,38 @@ buttonUp.addEventListener("click", uploadGif);
 
 function recGif() {
     debugger;
+    timer.style.display = "none";
     pasos[0].classList.remove("button-act");
     pasos[1].classList.add("button-act");
+    timer.style.display = "block";
     buttonRec.style.display = "none";
     buttonEnd.style.display = "block";
+    comienzo = new Date().getTime();
+    (function looper() {
+        timer.innerText = calculoDuraciónTiempo((new Date().getTime() - comienzo) / 1000);
+        setTimeout(looper, 1000);
+    })();
     recorder.startRecording();
     console.log("Record Ini");
 }
 
 async function stopRec() {
+    debugger;
+    video.pause();
     buttonEnd.style.display = "none";
     buttonUp.style.display = "block";
     pasos[1].classList.remove("button-act");
     pasos[2].classList.add("button-act");
-
     const sleep = m => new Promise(r => setTimeout(r, m));
     await sleep(3000);
-
     recorder.stopRecording(function () {
         let blob = recorder.getBlob();
         //invokeSaveAsDialog(blob);
         form.append('file', blob, "migGIFO.gif");
         form.append('api_key', apiKey);
     });
+    timer.style.display = "none";
+    repeatGif.style.display = "block";
     console.log(form.get('file'));
 }
 
@@ -88,4 +103,17 @@ async function uploadGif() {
             console.log(localStorage.getItem("misGIFOS"));
         })
         .catch(err => console.log(err));
+}
+
+function calculoDuraciónTiempo(seg) {
+    let horas = Math.floor(seg / 3600);
+    let minutos = Math.floor((seg - (horas * 3600)) / 60);
+    let segundos = Math.floor(seg - (horas * 3600) - (minutos * 60));
+    if (minutos < 10) {
+        minutos = "0" + minutos;
+    }
+    if (segundos < 10) {
+        segundos = "0" + segundos;
+    }
+    return horas + ":" + minutos + ":" + segundos;
 }
